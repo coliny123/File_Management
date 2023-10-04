2b2ebed5cf5f56e795b6b268538275c9e8640ae8
JiSeong Kim <12180543@inha.edu>
2023-10-05 01:45:13 +0900

    Feat: 네이버 로그인 기능 구현
    네이버 로그인 기능을 구현하였고 현재 버튼 스타일은 구현하지 않았음.

--- FE/front/src/components/btn/NaverLoginBtn.jsx
+++ FE/front/src/components/btn/NaverLoginBtn.jsx
@@ -2,6 +2,19 @@ import React from 'react'
 
 function NaverLoginBtn() {
 
+    const Naver_Client_ID = process.env.REACT_APP_Naver_Client_ID;
+    const redirect_uri = 'http://localhost:3001/naver/oauth'
+    const naver_uri = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${Naver_Client_ID}&redirect_uri=${redirect_uri}&state=false`;
+
+    const loginNaver = () => {
+        window.location.href = naver_uri;
+    }
+
+    return (
+        <div>
+            <button onClick={loginNaver}>네이버 로그인</button>
+        </div>
+    )
 }
 
 export default NaverLoginBtn
--- FE/front/src/pages/LandingPage.jsx
+++ FE/front/src/pages/LandingPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import GoogleLoginBtn from '../components/btn/GoogleLoginBtn'
+import NaverLoginBtn from '../components/btn/NaverLoginBtn'
 
 function LandingPage() {
 
   return (
     <div>
         <GoogleLoginBtn></GoogleLoginBtn>
+        <NaverLoginBtn></NaverLoginBtn>
     </div>
   )
 }
