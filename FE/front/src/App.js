f9f633c48d81bc8c35cadc0d2f2c58628fea6d4f
JiSeong Kim <12180543@inha.edu>
2023-10-08 01:57:33 +0900

    Fix: 라우터가 정상적으로 동작하지 않는 현상 해결
    Routes안에 Route만 배치함으로 정상적인 라우팅이 일어나도록 해결함.

--- FE/front/src/App.js
+++ FE/front/src/App.js
@@ -3,23 +3,23 @@ import './App.css';
 import LandingPage from './pages/LandingPage';
 import Navbar from './components/bar/Navbar';
 import Sidebar from './components/bar/Sidebar';
-import { Routes, Route, Router } from 'react-router-dom';
+import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Routes>
-          <Sidebar></Sidebar>
+        <Sidebar></Sidebar>
           <div className='ml-56 h-screen'>
             <Navbar></Navbar>
             <div className='page-area rounded-tl-3xl p-10 bg-[#F7F6FB] h-full'>
-              <Route path="/" element={<LandingPage />}></Route>
-              <Route path="/login" element={<LoginPage />}></Route>
+              <Routes>
+                <Route path="/" element={<LandingPage />}></Route>
+                <Route path="/login" element={<LoginPage />}></Route>
+              </Routes>
             </div>
           </div>
-        </Routes>
       </Router>
     </div>
   );
