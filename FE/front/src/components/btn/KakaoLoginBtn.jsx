e08e125e293ca61598d87181e39439dcc5ca076c
JiSeong Kim <12180543@inha.edu>
2023-10-08 22:18:13 +0900

    Chore: 리액트 쿼리 설치, api와 hooks 폴더 생성

--- FE/front/src/components/btn/KakaoLoginBtn.jsx
+++ FE/front/src/components/btn/KakaoLoginBtn.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 export default function KakaoLoginBtn() {
   
   const Kakao_Client_ID = process.env.REACT_APP_Kakao_Client_ID;
-  const redirect_uri = 'http://localhost:3001/kakao/oauth';
+  const redirect_uri = 'http://localhost:3000/kakao/oauth';
   const kakao_uri = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${Kakao_Client_ID}&redirect_uri=${redirect_uri}`;
-  console.log(kakao_uri)
+
   const loginKakao = () => {
     window.location.href = kakao_uri;
   }
