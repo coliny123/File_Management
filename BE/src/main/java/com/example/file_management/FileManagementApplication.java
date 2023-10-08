af57073b3d8a7359f94c38fc134c275a1cdc22b4
poow810 <poow810@naver.com>
2023-10-08 15:38:10 +0900

    배포 테스트

--- BE/src/main/java/com/example/file_management/FileManagementApplication.java
+++ BE/src/main/java/com/example/file_management/FileManagementApplication.java
@@ -2,9 +2,10 @@ package com.example.file_management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 @ConfigurationPropertiesScan
 public class FileManagementApplication {
 
