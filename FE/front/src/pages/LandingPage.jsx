70feff3d351fab5cca12fc9d3888a37059502780
JiSeong Kim <12180543@inha.edu>
2023-10-10 19:54:40 +0900

    Deploy Test

--- FE/front/src/pages/LandingPage.jsx
+++ FE/front/src/pages/LandingPage.jsx
@@ -11,6 +11,7 @@ function LandingPage() {
   // console.log(user)
   return (
     <div>
+      배포
         <FileStatusbar></FileStatusbar>
         <FileDragDrop></FileDragDrop>
         <QrCode url='http://192.168.0.8:3000/download/1'></QrCode>
