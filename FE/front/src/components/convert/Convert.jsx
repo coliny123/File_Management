e3c0345cf737e9bd3624380b13f54fcfa9946239
JiSeong Kim <12180543@inha.edu>
2023-10-15 10:13:02 +0900

    Chore: mui라이브러리 설치
    npm install @mui/material @emotion/react @emotion/styled 를 입력하여 설치

--- FE/front/src/components/convert/Convert.jsx
+++ FE/front/src/components/convert/Convert.jsx
@@ -15,10 +15,13 @@ function IsNotLoginConvert() {
 
 function IsLoginConvert() {
     return (
-        <div>
-            <div>여기는 변환 페이지</div>
-            <div>원래 형식 - 나중 형식</div>
-            <div>변환 후 보내는 버튼</div>
+        <div className='flex flex-col justify-center items-center'>
+            <div className='w-[161px]'>
+                <div className='text-2xl font-bold mt-5'>파일 변환</div>
+                <div className='border-2 w-full'>올린 파일</div>
+                <div>원래 형식 - 나중 형식</div>
+                <div>변환 후 보내는 버튼</div>
+            </div>
         </div>
     )
 }
@@ -29,7 +32,8 @@ function Convert() {
     return (
         <div className='w-full h-full flex flex-col justify-center items-center'>
             <div className='w-full h-full border-2'>
-                {isLogin ? <IsLoginConvert></IsLoginConvert> : <IsNotLoginConvert></IsNotLoginConvert>}
+                {/* {isLogin ? <IsLoginConvert></IsLoginConvert> : <IsNotLoginConvert></IsNotLoginConvert>} */}
+                {!isLogin ? <IsLoginConvert></IsLoginConvert> : <IsNotLoginConvert></IsNotLoginConvert>}
             </div>
             <div className='btns bg-blue-500 text-white w-[160px] h-[56px] mt-10 flex justify-center items-center'>
                 <button>Next</button>
