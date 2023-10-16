e44d64eadad0a95d01c7202cf4ec7fb11cb0b9bb
JiSeong Kim <12180543@inha.edu>
2023-10-16 16:13:14 +0900

    Feat: 파일 보내는 과정을 FileStatusbar에 추가
    FileStatusbar에 Convert 다음에 Sending을 추가함으로 파일 전송이 얼마나 진행되었는지 알 수 있도록 하는 과정을 추가함

--- FE/front/src/components/bar/FileStatusbar.jsx
+++ FE/front/src/components/bar/FileStatusbar.jsx
@@ -7,27 +7,26 @@ function FileStatusbar() {
     const {uploadStatus, setUploadStatus} = useUpload();
     
     const handleState = () => {
-        setUploadStatus((pre) => pre === 2 ? 0 : pre + 1)
-        console.log(uploadStatus)
+        setUploadStatus((pre) => pre === 3 ? 0 : pre + 1)
     }
-    const [nowState, setNowState] = useState(0)
 
     return (
         <div>
             <div className='wrapper flex justify-between'>
                 <div className='content flex flex-col items-center'>
-                    {/* <div className={`uploadStatus rounded-full w-3 h-3 ${nowState === 0 ? 'bg-blue-500' : ''} border-bg-500`}></div> */}
                     <div className={`uploadStatus rounded-full w-3 h-3 ${uploadStatus === 0 ? 'bg-blue-500' : ''} border-bg-500`}></div>
                     <div>upload</div>
                 </div>
                 <div className='content flex flex-col items-center'>
                     <div className={`uploadStatus rounded-full w-3 h-3 ${uploadStatus === 1 ? 'bg-blue-500' : ''} border-bg-500`}></div>
-                    {/* <div className={`uploadStatus rounded-full w-3 h-3 ${nowState === 1 ? 'bg-blue-500' : ''} border-bg-500`}></div> */}
                     <div>Convert</div>
                 </div>
                 <div className='content flex flex-col items-center'>
-                    {/* <div className={`uploadStatus rounded-full w-3 h-3 ${nowState === 2 ? 'bg-blue-500' : ''} border-bg-500`}></div> */}
                     <div className={`uploadStatus rounded-full w-3 h-3 ${uploadStatus === 2 ? 'bg-blue-500' : ''} border-bg-500`}></div>
+                    <div>Sending</div>
+                </div>
+                <div className='content flex flex-col items-center'>
+                    <div className={`uploadStatus rounded-full w-3 h-3 ${uploadStatus === 3 ? 'bg-blue-500' : ''} border-bg-500`}></div>
                     <div>Share</div>
                 </div>
             </div>
