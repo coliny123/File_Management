5cd0ba0f069e2fcd9508666010cbeff19e7a46eb
JiSeong Kim <12180543@inha.edu>
2023-10-19 22:23:05 +0900

    Design: font 적용 / pretendard-regular

--- FE/front/src/components/bar/Navbar.jsx
+++ FE/front/src/components/bar/Navbar.jsx
@@ -8,7 +8,7 @@ const LoginBtn = () => {
 
     const navigate = useNavigate();
     return (
-        <button className='bg-[#F7F6FB] w-[120px] h-[40px] rounded-full' onClick={() => navigate("/login")}>로그인</button>
+        <button className='text-white bg-[#6367EB] w-[160px] h-[48px] rounded-[18px]' onClick={() => navigate("/login")}>로그인</button>
     )
 }
 
@@ -22,7 +22,7 @@ const LogoutBtn = () => {
     }
 
     return (
-        <button className='bg-[#F7F6FB] w-[120px] h-[40px] rounded-full' onClick={logout}>로그아웃</button>
+        <button className='text-white bg-[#6367EB] w-[160px] h-[48px] rounded-[18px]' onClick={logout}>로그아웃</button>
     )
 }
 
@@ -35,9 +35,13 @@ function Navbar() {
     const {isLogin, setIsLogin} = useIsLogin();
     return (
         <div className='relative z-30 w-full h-[80px] bg-white flex items-center'>
-            <button onClick={() => navigate('/')}><div className={`ml-[16px] w-[48px] h-[48px] bg-[#F7F6FB] rounded-[14px] flex justify-center items-center text-2xl shadow-customIconBox text-${location.pathname.includes('file') ? '[#999999]' : '[#6367EB]'}`}><HiMiniHome/></div></button>
-            <input className='absolute left-[80px] bg-[#F7F6FB] text-center w-[160px] h-[40px] rounded-full' placeholder='코드 입력'></input>
-            <div onClick={() => navigate('/')} className='hover:cursor-pointer absolute left-1/2 -translate-x-1/2 text-center bg-[#F7F6FB] w-[160px] h-[40px] flex items-center justify-center'><BsCloudArrowDown className='text-black' /><p>ICloduU</p></div>
+            <button onClick={() => navigate('/')}>
+                <div className={`ml-[16px] w-[48px] h-[48px] bg-[#F7F6FB] rounded-[14px] flex justify-center items-center text-2xl shadow-customIconBox ${location.pathname.includes('file') ? 'text-[#999999]' : 'text-[#6367EB]'}`}>
+                    <HiMiniHome />
+                </div>
+            </button>
+            <input className='absolute left-[156px] bg-[#F7F6FB] text-center w-[200px] h-[48px] rounded-full' placeholder='코드 입력'></input>
+            {/* <div onClick={() => navigate('/')} className='hover:cursor-pointer absolute left-1/2 -translate-x-1/2 text-center bg-[#F7F6FB] w-[160px] h-[40px] flex items-center justify-center'><BsCloudArrowDown className='text-black' /><p>ICloduU</p></div> */}
             <div className='absolute right-10'>{isLogin ? <LogoutBtn /> : <LoginBtn />}</div>
         </div>
     )
