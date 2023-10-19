236a7d624208e8bbcbad85d5e8648f073bb833e0
ybg1219 <12202057@inha.edu>
2023-10-19 22:40:24 +0900

    conflict 해결

--- FE/front/src/components/bar/Navbar.jsx
+++ FE/front/src/components/bar/Navbar.jsx
@@ -1,13 +1,13 @@
 import React, { useState, useContext } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useIsLogin } from '../../context/IsLoginContext';
-import { BsCloudArrowDown } from 'react-icons/bs'
+import { BsFillCloudArrowUpFill } from 'react-icons/bs'
 
 const LoginBtn = () => {
 
     const navigate = useNavigate();
     return (
-        <button className='bg-[#F7F6FB] w-[120px] h-[40px] rounded-full' onClick={() => navigate("/login")}>로그인</button>
+        <button className='bg-[#F7F6FB] w-[120px] h-[40px] rounded-[16px]' onClick={() => navigate("/login")}>로그인</button>
     )
 }
 
@@ -21,7 +21,7 @@ const LogoutBtn = () => {
     }
 
     return (
-        <button className='bg-[#F7F6FB] w-[120px] h-[40px] rounded-full' onClick={logout}>로그아웃</button>
+        <button className='bg-[#F7F6FB] w-[120px] h-[40px] rounded-[16px]' onClick={logout}>로그아웃</button>
     )
 }
 
@@ -33,8 +33,8 @@ function Navbar() {
     const {isLogin, setIsLogin} = useIsLogin();
     return (
         <div className='relative z-30 w-full h-20 bg-white flex items-center'>
-            <input className='absolute left-[80px] bg-[#F7F6FB] text-center w-[160px] h-[40px] rounded-full' placeholder='코드 입력'></input>
-            <div onClick={() => navigate('/')} className='hover:cursor-pointer absolute left-1/2 -translate-x-1/2 text-center bg-[#F7F6FB] w-[160px] h-[40px] flex items-center justify-center'><BsCloudArrowDown className='text-black' /><p>ICloduU</p></div>
+            <input className='absolute left-[80px] bg-[#F7F6FB] text-center w-[200px] h-[40px] rounded-[16px]' placeholder='코드 입력'></input>
+            <div onClick={() => navigate('/')} className='hover:cursor-pointer absolute left-1/2 -translate-x-1/2 text-center bg-[#F7F6FB] w-[160px] h-[40px] flex items-center justify-center'><BsFillCloudArrowUpFill className='text-black' /><p>ICloduU</p></div>
             <div className='absolute right-10'>{isLogin ? <LogoutBtn /> : <LoginBtn />}</div>
         </div>
     )
