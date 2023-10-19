337ec8d0da7d9fa89662e77f66a0710e817f414f
JiSeong Kim <12180543@inha.edu>
2023-10-19 19:58:39 +0900

    Feat: 파일 관리 페이지 qrcode를 filedownload 페이지에 mapping
    파일 관리 페이지에서 각각의 fildId를 통한 qrcode를 생성해 fileDownload 페이지로 연결되도록 함

--- FE/front/src/components/fileList/FileList.jsx
+++ FE/front/src/components/fileList/FileList.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react'
-
+import { QRCodeCanvas } from 'qrcode.react';
 
 const fileInfoRow = (fileInfo, idx, handleTogglebar) => {
     return(
-        <div onClick={() => handleTogglebar(fileInfo)} className={`flex justify-around hover:cursor-pointer ${idx !== 0 ? 'border-t border-b' : ''}`} >
+        <div key={idx} onClick={() => handleTogglebar(fileInfo)} className={`flex justify-around hover:cursor-pointer ${idx !== 0 ? 'border-t border-b' : ''}`} >
             <div>{fileInfo?.fileName}</div>
             <div>{fileInfo?.fileUploadedDate}</div>
             <div>{fileInfo?.fileSharePermission}</div>
@@ -21,9 +21,9 @@ function FileList({fileInfoList}) {
         if (!isTogglebarOpen) {
             setIsTogglebarOpen(true);
             setTogglebarDataInfo(fileInfo)
-        } else if (togglebarDataInfo === fileInfo) {
+        } else if (isTogglebarOpen && togglebarDataInfo === fileInfo) {
             setIsTogglebarOpen(false);
-        } else if (togglebarDataInfo !== fileInfo) {
+        } else if (isTogglebarOpen && togglebarDataInfo !== fileInfo) {
             setTogglebarDataInfo(fileInfo)
         }
     }
@@ -49,6 +49,7 @@ function FileList({fileInfoList}) {
         <div className={`z-10 fixed top-[80px] right-0 flex-col md:w-80 w-60 bg-green-500 h-full transition-transform ease-in-out duration-300 transform ${isTogglebarOpen ? '' : 'translate-x-full'}`}>
             {togglebarDataInfo?.fileName}
             <div onClick={() => setIsTogglebarOpen(false)}>닫기버튼</div>
+                <QRCodeCanvas value={`http://192.168.45.81:3000/download/${togglebarDataInfo?.fileId}`}></QRCodeCanvas>
             <div>토글</div>
             <div>토글</div>
             <div>토글</div>
