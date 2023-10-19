84c0ca5e29daae30a8af9705fd6aee2ec401e3fd
poow810 <poow810@naver.com>
2023-10-19 15:55:01 +0900

    update

---
+++ BE/src/main/java/com/example/file_management/config/AppConfig.java
@@ -0,0 +1,18 @@
+package com.example.file_management.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunctions;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public WebClient webClient() {
+//        return WebClient.create();
+
+        return WebClient.builder()
+                .filter(ExchangeFilterFunctions.basicAuthentication("a6f22a74964f7bed67c0c5713fb357f9", "UhbeLsSE1g6JzEN0UCtOqhEMXy5hbV4c"))
+                .build();
+    }
+}
