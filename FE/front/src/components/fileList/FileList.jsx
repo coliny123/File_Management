2897c5b9f3d88552f063a66308aa8b5de41408ca
JiSeong Kim <12180543@inha.edu>
2023-10-19 17:57:51 +0900

    Feat: FileManagement 페이지 토글바 추가
    파일 목록을 누르면 오른쪽에서 토글바가 튀어나오는 코드 추가

--- FE/front/src/components/fileList/FileList.jsx
+++ FE/front/src/components/fileList/FileList.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
-import 
+import React, { useState } from 'react'
 
-const fileInfoRow = (fileInfo) => {
+const fileInfoRow = (fileInfo, idx, handleTogglebar) => {
     return(
-        <div onClick={} className='flex justify-around'>
+        <div onClick={() => handleTogglebar(fileInfo)} className='flex justify-around'>
             <div>{fileInfo?.fileName}</div>
             <div>{fileInfo?.fileUploadedDate}</div>
             <div>{fileInfo?.fileSharePermission}</div>
@@ -12,7 +11,16 @@ const fileInfoRow = (fileInfo) => {
 }
 
 function FileList({fileInfoList}) {
-  return (
+  
+    const [isTogglebarOpen, setIsTogglebarOpen] = useState(false);
+    const [togglebarDataInfo, setTogglebarDataInfo] = useState({});
+    
+    const handleTogglebar = (fileInfo) => {
+       setTogglebarDataInfo(fileInfo)
+        setIsTogglebarOpen(true)
+    }
+
+    return (
     <div>
       <div className='flex justify-around'>
         <div>파일명</div>
@@ -21,9 +29,23 @@ function FileList({fileInfoList}) {
       </div>
       <div>
         {fileInfoList.map((fileInfo, idx) =>
-            fileInfoRow(fileInfo)
+            fileInfoRow(fileInfo, idx, handleTogglebar)
         )}
       </div>
+        {/* <div className={`z-10 fixed top-0 right-24 flex-col w-24 bg-green-500 h-full transition-transform ease-in-out duration-300 transform ${isTogglebarOpen ? '' : '-translate-x-full'}`}>
+            {togglebarDataInfo?.fileName}
+            <div>토글</div>
+            <div>토글</div>
+            <div>토글</div>
+        </div> */}
+        <div className={`z-10 fixed top-[80px] right-0 flex-col md:w-80 w-60 bg-green-500 h-full transition-transform ease-in-out duration-300 transform ${isTogglebarOpen ? '' : 'translate-x-full'}`}>
+            {togglebarDataInfo?.fileName}
+            <div onClick={() => setIsTogglebarOpen(false)}>닫기버튼</div>
+            <div>토글</div>
+            <div>토글</div>
+            <div>토글</div>
+        </div>
+
     </div>
   )
 }
