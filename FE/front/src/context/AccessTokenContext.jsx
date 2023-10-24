285b8eafe7a499f7fa579bd594fe5c2b0916ad50
JiSeong Kim <12180543@inha.edu>
2023-10-24 21:24:48 +0900

    Feat: AccessTokenContext에 서버로 refreshToken을 보내 액세스 토큰을 갱신하는 함수를 구현
    localStorage에 저장된 refreshToken을 서버로 보내 액세스 토큰을 갱신하는 기능을 함.

--- FE/front/src/context/AccessTokenContext.jsx
+++ FE/front/src/context/AccessTokenContext.jsx
@@ -1,10 +1,16 @@
 import React, { createContext, useContext, useState } from 'react';
+import axios from 'axios';
 
 const AccessTokenContext = createContext();
 
 export function AccessTokenProvider({ children }) {
 
     const [accessToken, setAccessToken] = useState('');
+
+    const updateAccessToken = async () => {
+        axios.post('서버 주소', {token: window.localStorage.get('refreshToken')})
+        .then((res) => setAccessToken(res.data.token))
+    }
 
     return (
         <AccessTokenContext.Provider value={{ accessToken, setAccessToken }}>
