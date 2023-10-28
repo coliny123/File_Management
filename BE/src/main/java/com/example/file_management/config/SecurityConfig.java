cbe6a1e776815002361f6f68ea1f9182b1413d3f
poow810 <poow810@naver.com>
2023-10-28 17:02:38 +0900

    fix: cors 설정 변경

--- BE/src/main/java/com/example/file_management/config/SecurityConfig.java
+++ BE/src/main/java/com/example/file_management/config/SecurityConfig.java
@@ -1,10 +1,14 @@
 package com.example.file_management.config;
 
+import java.util.Arrays;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -14,8 +18,19 @@ public class SecurityConfig {
         http.authorizeRequests()
                 .anyRequest().permitAll()  // 모든 요청 허용
                 .and()
-                .csrf(csrf -> csrf.disable());  // CSRF 공격 방어 기능 비활성화
+                .csrf(csrf -> csrf.disable())  // CSRF 공격 방어 기능 비활성화
+                .cors().configurationSource(corsConfigurationSource());
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://file-management-ten.vercel.app"));  // 허용할 도메인
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));  // 허용할 HTTP 메소드
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
