f655c74c63bace177b4b20f737d7ca999c9e3783
JiSeong Kim <12180543@inha.edu>
2023-10-29 15:17:32 +0900

    release test

--- FE/front/src/components/btn/KakaoLoginBtn.jsx
+++ FE/front/src/components/btn/KakaoLoginBtn.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 export default function KakaoLoginBtn() {
   
   const Kakao_Client_ID = process.env.REACT_APP_Kakao_Client_ID;
-  const redirect_uri = 'http://localhost:3000/auth/kakao/callback';
+  // const redirect_uri = 'http://localhost:3000/auth/kakao/callback';
+  const redirect_uri = 'https://filemanager.kro.kr/auth/kakao/callback';
   const kakao_uri = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${Kakao_Client_ID}&redirect_uri=${redirect_uri}`;
 
   const loginKakao = () => {
