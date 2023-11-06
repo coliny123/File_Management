ff4f95986e4fa3ac9ba83d083a0d084f038a3d7b
JiSeong Kim <12180543@inha.edu>
2023-11-06 21:33:41 +0900

    release test

--- FE/front/src/pages/FileDownloadPage.jsx
+++ FE/front/src/pages/FileDownloadPage.jsx
@@ -13,8 +13,8 @@ function FileDownloadPage() {
   const getFileInfo = async (fileId) => {
     try {
       const response = await axios.get(`${Server_IP}/download/${fileId}`);
-      setFileLink(response.data)
-      console.log(response.data)
+      setFileLink(response.data.data)
+      console.log(response.data.data)
     } catch (error) {
       console.error('Error fetching data:', error);
     }
