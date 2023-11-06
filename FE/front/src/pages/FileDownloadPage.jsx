229c5fa1a228edc6d19025eb0612fd4ac30e2477
JiSeong Kim <12180543@inha.edu>
2023-11-06 21:30:26 +0900

    release test

--- FE/front/src/pages/FileDownloadPage.jsx
+++ FE/front/src/pages/FileDownloadPage.jsx
@@ -2,21 +2,18 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import axios from 'axios';
 
-
 function FileDownloadPage() {
 
   const Server_IP = process.env.REACT_APP_Server_IP;
-  
+  const [fileLink, setFileLink] = useState('');
+  const 
   // useEffect로 api / 파일 id를 보내서 다운로드 링크를 받아옴
   // useEffect로 지금 현재 파일 다운 가능한지 서버로 요청 보내봐야함
-  
 	const { fileId } = useParams();
-  const [fileLink, setFileLink] = useState('');
-
+  
   const getFileInfo = async (fileId) => {
     try {
       const response = await axios.get(`${Server_IP}/download/${fileId}`);
-      console.log(response);
       setFileLink(response.data)
     } catch (error) {
       console.error('Error fetching data:', error);
@@ -24,15 +21,13 @@ function FileDownloadPage() {
   }
   
   useEffect(() => {
-    console.log(fileLink);
     getFileInfo(fileId);
-    console.log(fileLink);
   }, [])
 
   return (
     <div>
       {`${fileId} 파일입니다.`}
-      <div className='bg-yellow-500'><button>download</button></div>
+      <div className='bg-yellow-500'><button onClick={() => {window.open(fileLink, '_blank')}}>download</button></div>
     </div>
   )
 }
