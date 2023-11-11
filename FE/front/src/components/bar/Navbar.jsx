e29cf18b73681687d740ef05d73ae2e399e33fcc
ybg1219 <12202057@inha.edu>
2023-11-12 01:24:15 +0900

    feat:파일 다운로드 페이지
    QR 찍었을때 생성되는 파일 다운로드 페이지의 디자인 구현

--- FE/front/src/components/bar/Navbar.jsx
+++ FE/front/src/components/bar/Navbar.jsx
@@ -5,6 +5,14 @@ import { BsCloudArrowDown } from 'react-icons/bs'
 import { HiMiniHome } from 'react-icons/hi2'
 import { AiFillCloud } from 'react-icons/ai'
 
+const DownBtn = () => {
+
+    const navigate = useNavigate();
+    return (
+        <button className='text-white bg-[#6367EB] w-full h-[48px] rounded-[18px]' onClick={() => navigate("/download/1")}>다운로드</button>
+    )
+}
+
 const LoginBtn = () => {
 
     const navigate = useNavigate();
@@ -44,6 +52,8 @@ function Navbar() {
                 </div>
             </button> */}
             <div className='absolute left-[156px] bg-[#F7F6FB] w-[200px] h-[48px] rounded-[18px] flex items-center font-bold pl-5'> <div className=''>코드입력:</div>  <input className='w-[100px] h-full rounded-[18px] pointer-events-auto bg-[#F7F6FB] outline-none' type="text"/></div>
+            <div className='absolute left-[360px] bg-[#F7F6FB] w-[200px] h-[48px] rounded-[18px] flex items-center pl-5'><DownBtn/> </div>
+            
             <div className='absolute right-[40px] w-[120px] h-[48px]'>{isLogin ? <LogoutBtn /> : <LoginBtn />}</div>
         </div>
 
