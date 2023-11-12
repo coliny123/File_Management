4546ea8244cc7b9a86e5f586c2f661adf1fa279c
poow810 <poow810@naver.com>
2023-11-12 23:00:58 +0900

    test : release test release test Resolves : #1

--- BE/src/main/java/com/example/file_management/config/SecurityConfig.java
+++ BE/src/main/java/com/example/file_management/config/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.example.file_management.config;
 
 import java.util.List;
-import org.springframework.boot.autoconfigure.security.reactive.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -18,15 +17,16 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .requestMatchers("/details/*", "/download/*").permitAll() // 특정 엔드포인트에 대해 인증 생략
-                .anyRequest().authenticated() // 나머지 모든 엔드포인트는 인증 요구
-                .and().csrf().disable().cors().configurationSource(request -> {
+                .anyRequest().permitAll()  // 모든 요청 허용
+                .and()
+                .csrf(AbstractHttpConfigurer::disable)  // CSRF 공격 방어 기능 비활성화
+                .cors(cors -> cors.configurationSource(request -> {
                     CorsConfiguration configuration = new CorsConfiguration();
-                    configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://file-management-ten.vercel.app")); // 허용할 도메인
-                    configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS")); // 허용할 HTTP 메소드
+                    configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://file-management-ten.vercel.app"));  // 허용할 도메인
+                    configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));  // 허용할 HTTP 메소드
                     configuration.setAllowedHeaders(List.of("*"));
                     return configuration;
-                });
+                }));
 
         return http.build();
     }
