2e5cd020b586a00887053cf442af3ed3a110c7ae
JiSeong Kim <12180543@inha.edu>
2023-11-12 17:54:11 +0900

    deploy test

--- FE/front/src/api/fetchUserDataApi.jsx
+++ FE/front/src/api/fetchUserDataApi.jsx
@@ -2,6 +2,7 @@ import axios from 'axios'
 
 export const fetchUserDataApi = async () => {
     const Server_IP = process.env.REACT_APP_Server_IP;
+    console.log(axios.defaults.headers)
     await axios.get(`${Server_IP}/users/files`)
         .then((res) => {
             console.log(res.data);
