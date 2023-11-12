6e888cf147e73f27a762833535133cd8a64863b0
leedabin2 <12193152@inha.edu>
2023-11-12 17:10:23 +0900

    refactor(TokenController): refreshAccessToken 메서드 코드 리팩토링

--- BE/src/main/java/com/example/file_management/oauth/TokenController.java
+++ BE/src/main/java/com/example/file_management/oauth/TokenController.java
@@ -1,54 +1,20 @@
 package com.example.file_management.oauth;
 
-import com.example.file_management.security.JwtUtil;
-import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import com.example.file_management.security.JwtValidator;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @RestController
-@CrossOrigin(origins="http://localhost:3000")
+@RequiredArgsConstructor
 public class TokenController {
-    private final JwtUtil jwtUtil;
-
-    @Autowired
-    public TokenController(JwtUtil jwtUtil) {
-        this.jwtUtil = jwtUtil;
-    }
+    private final JwtValidator jwtValidator;
 
     @GetMapping("/auth/refresh")
-    public ResponseEntity<?> refreshAccessToken(@RequestHeader(value="Authorization") String refreshToken, HttpServletResponse response) {
-        refreshToken = refreshToken.replace("Bearer ", ""); // 토큰에서 "Bearer " 문자열 제거
-
-        // DB에서 리프레시 토큰을 조회
-        String refreshTokenInDb = jwtUtil.getRefreshTokenFromToken(refreshToken);
-
-        if (refreshTokenInDb == null) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("유효하지 않는 refresh token 입니다.");
-        }
-
-        // DB에 저장된 리프레시 토큰 유효성 검사
-        boolean isValid = jwtUtil.validateRefreshToken(refreshTokenInDb);
-
-        if (!isValid) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("유효하지 않는 refresh token 입니다.");
-        }
-
-        // 새로운 엑세스 토큰 생성
-        String newAccessToken = jwtUtil.generateAccessTokenFromRefreshToken(refreshTokenInDb);
-
-        // 응답 본문에 새로운 엑세스 토큰을 담아서 클라이언트에 전송
-        Map<String, String> responseBody = new HashMap<>();
-        responseBody.put("accessToken", newAccessToken);
-
-        return ResponseEntity.ok(responseBody);
+    public ResponseEntity<?> refreshAccessToken(@RequestHeader(value="Authorization") String refreshToken) {
+        return jwtValidator.validateAndGenerateAccessToken(refreshToken);
     }
 }
 
