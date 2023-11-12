4c878dbba71c63dc035b00ebad743a1a4ffd26ec
leedabin2 <12193152@inha.edu>
2023-11-12 17:12:28 +0900

    feat(JwtValidator):  refreshAccessToken 메서드 코드 분리
    refreshAccessToken(TokenController) 메서드에 대한 코드들을 리팩토링하면서 JwtValidator클래스에 메서드 분리한 뒤 기능을 추가했습니다.

--- BE/src/main/java/com/example/file_management/security/JwtValidator.java
+++ BE/src/main/java/com/example/file_management/security/JwtValidator.java
@@ -1,12 +1,18 @@
 package com.example.file_management.security;
 
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Component
+@RequiredArgsConstructor
 public class JwtValidator {
+    private final JwtUtil jwtUtil;
 
     public boolean validateAccessToken(String token) {
         token = token.replace("Bearer ", ""); // 토큰에서 "Bearer " 문자열 제거
@@ -25,5 +31,28 @@ public class JwtValidator {
         String token = request.getHeader("Authorization");
         return checkTokenValidity(token);
     }
+
+    // 리프레시 토큰의 유효성을 검사하고 새로운 엑세스 토큰을 발급
+    public String removeBearerFromToken(String refreshToken) {
+        return refreshToken.replace("Bearer ", "");
+    }
+
+    public ResponseEntity<?> validateAndGenerateAccessToken(String refreshToken) {
+        refreshToken = removeBearerFromToken(refreshToken);
+
+        String refreshTokenInDb = jwtUtil.getRefreshTokenFromToken(refreshToken);
+
+        if (refreshTokenInDb == null || !JwtUtil.validateRefreshToken(refreshTokenInDb)) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("유효하지 않는 refresh token 입니다.");
+        }
+
+        String newAccessToken = jwtUtil.generateAccessTokenFromRefreshToken(refreshTokenInDb);
+
+        Map<String, String> responseBody = new HashMap<>();
+        responseBody.put("accessToken", newAccessToken);
+
+        return ResponseEntity.ok(responseBody);
+    }
 }
+
 
