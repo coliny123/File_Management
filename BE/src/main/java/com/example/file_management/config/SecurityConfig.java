7e558d97902758045c0ed0ffcc15823060e4521b
leedabin2 <12193152@inha.edu>
2023-11-12 21:49:29 +0900

    feat(SecurityConfig):  JWT 필터를 Spring Security 필터 체인에 추가하는 역할인 addFilterBefore() 메소드 추가
    모든 요청은 JWT 필터를 거친 후에 처리되며, JWT 토큰의 유효성 검사는 JWT 인증 공급자에서 수행됩니다.

--- BE/src/main/java/com/example/file_management/config/SecurityConfig.java
+++ BE/src/main/java/com/example/file_management/config/SecurityConfig.java
@@ -1,33 +1,54 @@
 package com.example.file_management.config;
 
-import java.util.List;
+import com.example.file_management.security.JwtAuthenticationFilter;
+import com.example.file_management.security.JwtAuthenticationProvider;
+import com.example.file_management.security.JwtValidator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 
 import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .anyRequest().permitAll()  // 모든 요청 허용
+    public JwtAuthenticationFilter jwtAuthenticationFilter(JwtValidator jwtValidator) {
+        return new JwtAuthenticationFilter(jwtValidator);
+    }
+
+    @Bean
+    public JwtAuthenticationProvider jwtAuthenticationProvider(JwtValidator jwtValidator) {
+        return new JwtAuthenticationProvider(jwtValidator);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http,
+                                                   JwtAuthenticationFilter jwtAuthenticationFilter,
+                                                   JwtAuthenticationProvider jwtAuthenticationProvider) throws Exception {
+        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .authenticationProvider(jwtAuthenticationProvider)
+                .authorizeRequests()
+                .anyRequest().permitAll() // 모든 요청 허용
                 .and()
-                .csrf(AbstractHttpConfigurer::disable)  // CSRF 공격 방어 기능 비활성화
+                .csrf(AbstractHttpConfigurer::disable) // CSRF 공격 방어 기능 비활성화
                 .cors(cors -> cors.configurationSource(request -> {
                     CorsConfiguration configuration = new CorsConfiguration();
-                    configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://file-management-ten.vercel.app"));  // 허용할 도메인
-                    configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));  // 허용할 HTTP 메소드
+                    configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://file-management-ten.vercel.app"));
+                    configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                     configuration.setAllowedHeaders(List.of("*"));
                     return configuration;
                 }));
 
         return http.build();
     }
+
+
 }
