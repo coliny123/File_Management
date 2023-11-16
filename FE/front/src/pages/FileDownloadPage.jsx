b5410a7c1874708b6117315997236f9c2245d378
JiSeong Kim <12180543@inha.edu>
2023-11-16 14:45:02 +0900

    Feat: filedonwloadPage 공유 권한 확인 후 다운로드 버튼에 적용

--- FE/front/src/pages/FileDownloadPage.jsx
+++ FE/front/src/pages/FileDownloadPage.jsx
@@ -27,7 +27,8 @@ function FileDownloadPage() {
 
     return (
     <div className='w-full h-screen bg-[#F7F6FB] flex flex-col items-center'>
-      <div text-neutral-600 className='font-bold mt-[80px]'> {`${fileId} 파일 다운로드`} </div>
+      <div text-neutral-600 className='font-bold mt-[80px]'> {`파일 다운로드`} </div>
+      {/* <div text-neutral-600 className='font-bold mt-[80px]'> {`${fileId} 파일 다운로드`} </div> */}
       <div className='m-[32px] flex flex-col'>
         {/* <div className='bg-[#ffffff] w-[120px] h-[160px] rounded-[6px] mt-[32px]'></div> */}
             <div className='w-[240px]'>
@@ -35,13 +36,13 @@ function FileDownloadPage() {
                 <div className='bg-[#ffffff] w-[120px] h-[160px] rounded-[6px] mt-[32px]'></div>
               </div>
               <div className='flex justify-between'><p>파일명</p><p>{fileData?.originalFileName}</p></div>
-              <div className='flex justify-between'><p>확장자</p><p>{}</p></div>
+              <div className='flex justify-between'><p>확장자</p><p>{fileData?.split('.')[1]}</p></div>
               <div className='flex justify-between'><p>올린 날짜</p><p>{fileData?.uploadTime?.split('.')[0]?.split('T')?.join(' ')}</p></div>
               <div className='flex justify-between'><p>공유 권한</p><p>{fileData?.shared === true ? '허용' : fileData?.shared === false ? '차단' : ''}</p></div>
               <div className='flex justify-between'><p>다운 코드</p><p>{fileData?.authenticationCode}</p></div>
             </div>
       </div>
-      <div className='text-white flex bg-[#6367EB] w-[200px] h-[48px] rounded-[4px] m-[32px] justify-center items-center'><button onClick={() => {window.location = fileData.savedPath}} disabled={!fileData.shared} >download</button></div>
+      <div className={`text-white flex bg-[#6367EB] w-[200px] h-[48px] rounded-[4px] m-[32px] justify-center items-center ${!fileData.shared ? 'opacity-50' : 'opacity-100'}`}><button onClick={() => {window.location = fileData.savedPath}} disabled={!fileData.shared} >download</button></div>
       {/* 글씨를 버튼 가운데로 하고 싶어요.. */}
     </div>
     )
