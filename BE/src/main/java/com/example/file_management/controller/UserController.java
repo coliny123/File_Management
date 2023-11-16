189c8461470e2092d435b04326997cd358a71cbc
leedabin2 <12193152@inha.edu>
2023-11-16 21:15:26 +0900

    style: 코드 주석 처리

--- BE/src/main/java/com/example/file_management/controller/UserController.java
+++ BE/src/main/java/com/example/file_management/controller/UserController.java
@@ -5,9 +5,10 @@ import com.example.file_management.security.JwtValidator;
 import com.example.file_management.service.UserInfoService;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
@@ -24,24 +25,21 @@ public class UserController {
         this.userInfoService = userInfoService;
         this.jwtValidator = jwtValidator;
     }
-    @GetMapping("/me")
-    public ResponseEntity<?> User (@RequestHeader(value="Authorization") String token) {
-        token = token.replace("Bearer ", ""); // 토큰에서 "Bearer " 문자열 제거
-
-        boolean isValid = jwtUtil.validateRefreshToken(token);
-
-        if (!isValid) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("유효하지 않는 access token 입니다.");
-        }
-
-        return ResponseEntity.ok("유효한 access token 입니다.");
-    }
+//    @GetMapping("/me")
+//    public ResponseEntity<?> User (@RequestHeader(value="Authorization") String token) {
+//        token = token.replace("Bearer ", ""); // 토큰에서 "Bearer " 문자열 제거
+//
+//        boolean isValid = jwtUtil.validateRefreshToken(token);
+//
+//        if (!isValid) {
+//            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("유효하지 않는 access token 입니다.");
+//        }
+//
+//        return ResponseEntity.ok("유효한 access token 입니다.");
+//    }
 
     @GetMapping("/files")
     public ResponseEntity<?> getUserFiles(HttpServletRequest request) {
-        ResponseEntity responseEntity = jwtValidator.validateRequestToken(request);
-        if (responseEntity != null) return responseEntity;
-
         return ResponseEntity.ok(userInfoService.getUserInfoAndFiles(request));
     }
 }
