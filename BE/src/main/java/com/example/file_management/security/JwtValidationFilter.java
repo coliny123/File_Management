03b7731ed6c2d9b084ae5c718cd8b0425f10e10b
leedabin2 <12193152@inha.edu>
2023-11-16 21:07:02 +0900

    feat: Spring Security가 해당 요청을 인증된 사용자의 요청으로 인식하는 인스턴스 생성

--- BE/src/main/java/com/example/file_management/security/JwtValidationFilter.java
+++ BE/src/main/java/com/example/file_management/security/JwtValidationFilter.java
@@ -1,27 +1,31 @@
 package com.example.file_management.security;
 
+import io.jsonwebtoken.Claims;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class JwtValidationFilter extends OncePerRequestFilter {
 
     private final JwtValidator jwtValidator;
-    private final List<String> pathsToAuth = Arrays.asList("/upload");
+    private final List<String> pathsToAuth = Arrays.asList("/upload","/users/files");
 
     public JwtValidationFilter(JwtValidator jwtValidator) {
         this.jwtValidator = jwtValidator;
     }
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String requestURI = request.getRequestURI();
         // 리스트에 포함된 엔드포인트에 대한 요청만 토큰을 검증
         if (pathsToAuth.contains(requestURI)) {
@@ -29,6 +33,14 @@ public class JwtValidationFilter extends OncePerRequestFilter {
             if (responseEntity != null) {
                 response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "권한이 없는 토큰입니다.");
                 return;
+            } else {
+                // 토큰이 유효하다면, SecurityContext에 인증 정보를 설정
+                String token = jwtValidator.extractToken(request);
+                Claims claims = jwtValidator.getClaimsFromToken(token);
+                UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(
+                        claims.getSubject(), null, new ArrayList<>()
+                );
+                SecurityContextHolder.getContext().setAuthentication(auth);
             }
         }
         filterChain.doFilter(request, response);
