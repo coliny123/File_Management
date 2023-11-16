38197c4b77c2d24b7ee2f36293d80d86c9a82479
JiSeong Kim <12180543@inha.edu>
2023-11-16 15:03:05 +0900

    release test

--- FE/front/src/api/getFileIdByAuthCode.jsx
+++ FE/front/src/api/getFileIdByAuthCode.jsx
@@ -5,7 +5,7 @@ export const getFileIdByAuthCode = async (authCode) => {
     await axios.get(`${Server_IP}/download/${authCode}`)
     .then((res) => {
         console.log(res)
-        return res.data.id
+        return res.data;
     })
     .catch((err) => {
         alert('코드를 다시 확인해주세요.')
