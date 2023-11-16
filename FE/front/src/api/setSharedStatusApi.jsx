cf22c7c7a79e3beb9d673d9802095df084059418
JiSeong Kim <12180543@inha.edu>
2023-11-16 15:43:36 +0900

    fix: setSharedStatus의 shared value 수정

--- FE/front/src/api/setSharedStatusApi.jsx
+++ FE/front/src/api/setSharedStatusApi.jsx
@@ -4,7 +4,7 @@ export function setSharedStatusApi(fileId, status) {
     const Server_IP = process.env.REACT_APP_Server_IP;
     axios.post(`${Server_IP}/setShared`, {
         id: fileId,
-        shared: status === 1 ? false : true,
+        shared: status,
     })
     .then((res) => console.log(res))
     .catch((err) => console.log(err))
