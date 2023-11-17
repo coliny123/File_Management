f14fdcf3351c469fe770d87413fd5a3c17b6a045
JiSeong Kim <12180543@inha.edu>
2023-11-18 01:24:34 +0900

    Design: 파비콘 변경

--- FE/front/src/components/bar/Navbar.jsx
+++ FE/front/src/components/bar/Navbar.jsx
@@ -61,7 +61,7 @@ function Navbar() {
     const {isLogin, setIsLogin} = useIsLogin();
     return (
         <div className='fixed z-[1000] w-full h-[80px] bg-white flex items-center'>
-            <div className='absolute left-[156px] bg-[#F7F6FB] w-[200px] h-[48px] rounded-[18px] flex items-center font-bold pl-5 max-md:invisible'> <div className=''>코드입력:</div><input className='w-[100px] h-full rounded-[18px] pointer-events-auto bg-[#F7F6FB] outline-none' type="text" value={downloadCode} onChange={(e) => setDownloadCode(e.target.value)} onKeyDown={(e) => {if (e.key === 'Enter') {handleDownloadButton()}}} /></div>
+            <div className='absolute left-[156px] bg-[#F7F6FB] w-[200px] h-[48px] rounded-[18px] flex items-center font-bold pl-5 max-md:invisible'> <div className='mr-3'>코드입력:</div><input className='w-[100px] h-full rounded-[18px] pointer-events-auto bg-[#F7F6FB] outline-none' type="text" value={downloadCode} onChange={(e) => setDownloadCode(e.target.value)} onKeyDown={(e) => {if (e.key === 'Enter') {handleDownloadButton()}}} /></div>
             <div className='absolute right-[40px] w-[120px] h-[48px]'>{isLogin ? <LogoutBtn /> : <LoginBtn />}</div>
         </div>
     )
