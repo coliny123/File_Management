b1ecb6afd8baefdcd3259f85113e092d7e15dd43
JiSeong Kim <12180543@inha.edu>
2023-11-18 18:46:55 +0900

    Design: 바텀바 경로 조건에 따른 디자인 변경 추가

--- FE/front/src/components/bar/Bottombar.jsx
+++ FE/front/src/components/bar/Bottombar.jsx
@@ -14,7 +14,7 @@ function Bottombar() {
         <>
             <div className='fixed bottom-0 w-full flex justify-center bg-white z-20 h-[80px] border-t-2'>
                 <div className='menu flex justify-center items-center text-[#999999] space-x-20'>
-                    <button onClick={() => navigate('/')}><div className={`w-[48px] h-[48px] bg-[#F7F6FB] rounded-[14px] flex justify-center items-center text-2xl shadow-customIconBox ${location.pathname.includes('file') ? 'text-[#999999]' : 'text-[#6367EB]'}`}><HiMiniHome /></div></button>
+                    <button onClick={() => navigate('/')}><div className={`w-[48px] h-[48px] bg-[#F7F6FB] rounded-[14px] flex justify-center items-center text-2xl shadow-customIconBox ${location.pathname === '/' ? 'text-[#6367EB]' : 'text-[#999999]'}`}><HiMiniHome /></div></button>
                     <button onClick={() => navigate('/file')}><div className='w-[48px] h-[48px] bg-[#F7F6FB] rounded-[14px] flex justify-center items-center text-2xl shadow-customIconBox'><HiFolderOpen/></div></button>
                     <button onClick={() => navigate('/code')}><div className='w-[48px] h-[48px] bg-[#F7F6FB] rounded-[14px] flex justify-center items-center text-2xl shadow-customIconBox'><FaCloudDownloadAlt/></div></button>
                 </div>
