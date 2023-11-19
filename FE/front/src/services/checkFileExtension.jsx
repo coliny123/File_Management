f0e0601e703d4c1f74a1e1de16b8f7bb2fe777ac
JiSeong Kim <12180543@inha.edu>
2023-11-19 23:40:08 +0900

    Fix: 한글 설치 되지 않은 기기에서 한글 업로드 시도

--- FE/front/src/services/checkFileExtension.jsx
+++ FE/front/src/services/checkFileExtension.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 export function checkFileExtension(fileList) {
     const fileFullExtension = fileList[0].type;
+    if (fileList[0].name.slice(-3) === 'hwp') {
+        return 'hwp'
+    }
     console.log(fileList[0].type)
     const fileExtensionObject = {
         'application/haansofthwp': 'hwp',
